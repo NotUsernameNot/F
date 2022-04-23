933ef9c5bc5f53fbefc7023a4c82714993f35536
NotUsernameNot <104234427+NotUsernameNot@users.noreply.github.com>
2022-04-23 02:02:38 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1141,6 +1141,9 @@ class PlayState extends MusicBeatState
 				case 'senpai' | 'roses' | 'thorns':
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
+				
+				case 'tutorial':
+					startDialogue(dialogueJson)
 
 				default:
 					startCountdown();
